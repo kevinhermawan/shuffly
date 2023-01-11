fc9deb9d645b41e0293b244ec28e521caa0b5e02
Kevin Hermawan <git@kevinx.io>
2023-01-11 19:46:56 +0700

    first commit

---
+++ test/index.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+
+import { shuffle, shuffleUnique } from '../src/index';
+
+const array = [1, 2, 3, 4, 5];
+
+describe('shuffly', () => {
+  it('shuffle', () => {
+    const shuffledArray = shuffle(array);
+
+    expect(Array.isArray(shuffledArray)).toBe(true);
+    expect(shuffledArray.length).toBe(array.length);
+    expect(shuffledArray).not.toEqual(array);
+  });
+
+  it('shuffleUnique', () => {
+    const shuffledArray = shuffleUnique(array);
+
+    expect(Array.isArray(shuffledArray)).toBe(true);
+    expect(shuffledArray.length).toBe(array.length);
+    expect(shuffledArray).not.toEqual(array);
+  });
+});
